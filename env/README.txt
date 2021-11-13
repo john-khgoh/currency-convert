f751e39572d3144bbf7f40d12d88bdc8120b093c
john-khgoh <john.khgoh@gmail.com>
2021-11-13 12:21:16 +0800

    updated

---
+++ env/README.txt
@@ -0,0 +1,5 @@
+1. Start the local webserver by using the following command in command prompt (Windows) or terminal (*nix). Change the host and port accordingly. 
+uvicorn main:app --reload --workers 1 --host 0.0.0.0 --port 8008
+
+2. Once started, open another cmd/terminal, and run the following command. Change the target currency and file, as required. 
+python cli.py --target-currency usd --file "input.json"
--- env/cli.py
+++ env/cli.py
@@ -3,23 +3,23 @@ import json
 import argparse
 from os import getcwd, name
 
+path = getcwd()
 http = urllib3.PoolManager()
 os = name
-website = '127.0.0.1'
-port = 8008
+website = '127.0.0.1:8008'
+#port = 8008
 currency = 'usd'
 target_currency = 'usd'
 value = 0
 separator = '\\'
 
 parser = argparse.ArgumentParser(description='Currency converter')
-parser.add_argument('--file', dest='file',help='Path to the input file.',required=True)
+parser.add_argument('--file', dest='file',help='Name of the input file.',required=True)
 parser.add_argument('--target-currency', dest='target_currency',help='Specify the target currency e.g. usd, eur, without inverted commas',required=True)
 args = vars(parser.parse_args())
 
 target_currency = args['target_currency']
 file = args['file']
-path = getcwd()
 
 #Different separator for Windows, *nix and MacOS
 if(os=='nt'):
@@ -28,16 +28,18 @@ else:
     separator='/'
 
 filepath = path + separator + file
+
+#Reads one line at a time, so should be able to handle very large json files
 with open(filepath) as f:
     for line in f:
         data = json.loads(line)
         value = data['value']
         currency = data['currency']
-        url = website + ':' + str(port) + '/' + 'convert' + '/' + currency + '/' + target_currency + '/' + str(value)
+        url = website + '/' + 'convert' + '/' + currency + '/' + target_currency + '/' + str(value)
         r = http.request('GET',url)
         output = r.data.decode('ascii')
         output = json.loads(output)
-        print(output)
+        print(output)   
         
 '''
 url = website + ':' + str(port) + '/' + 'convert' + '/' + currency + '/' + target_currency + '/' + str(value)
