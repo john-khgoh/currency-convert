8c04c3e401d251fe5296e0e0cdd25c5670311939
john-khgoh <john.khgoh@gmail.com>
2021-11-13 16:58:14 +0800

    added try-except handling for erratic input.json

--- cli.py
+++ cli.py
@@ -6,13 +6,15 @@ from os import getcwd, name
 path = getcwd()
 http = urllib3.PoolManager()
 os = name
-website = '127.0.0.1:8008'
-#website = 'https://currency-convert-1.herokuapp.com/'
+#website = '127.0.0.1:8008' #localhost
+website = 'https://currency-convert-1.herokuapp.com'
 currency = 'usd'
 target_currency = 'usd'
+cnt = 0
 value = 0
 separator = '\\'
 
+#Get arguments required to run
 parser = argparse.ArgumentParser(description='Currency converter')
 parser.add_argument('--file', dest='file',help='Name of the input file.',required=True)
 parser.add_argument('--target-currency', dest='target_currency',help='Specify the target currency e.g. usd, eur, without inverted commas',required=True)
@@ -29,25 +31,18 @@ else:
 
 filepath = path + separator + file
 
-#Reads one line at a time, so should be able to handle very large json files
+#Reads one line at a time without storing it in memory. It should be able to handle very large JSON files.
 with open(filepath) as f:
     for line in f:
-        data = json.loads(line)
-        value = data['value']
-        currency = data['currency']
-        url = website + '/' + 'convert' + '/' + currency + '/' + target_currency + '/' + str(value)
-        r = http.request('GET',url)
-        output = r.data.decode('ascii')
-        output = json.loads(output)
-        print(output)   
-        
-'''
-url = website + ':' + str(port) + '/' + 'convert' + '/' + currency + '/' + target_currency + '/' + str(value)
-
-http = urllib3.PoolManager()
-#r = http.request('GET', '127.0.0.1:8008/convert/usd/usd/1.0')
-r = http.request('GET',url)
-output = r.data.decode('ascii')
-output = json.loads(output)
-print(output)
-'''
+        cnt += 1
+        try:
+            data = json.loads(line)
+            value = data['value']
+            currency = data['currency']
+            url = website + '/' + 'convert' + '/' + currency + '/' + target_currency + '/' + str(value)
+            r = http.request('GET',url)
+            output = r.data.decode('ascii')
+            output = json.loads(output)
+            print(output)
+        except:
+            print("Error reading %s at line %d" %(file,cnt)) #Notifies if there are errors with the input file, but continues with other lines
